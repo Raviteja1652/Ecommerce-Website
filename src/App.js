c8090af92fc626987ff0397b1a82cfb29b143f10
Raviteja1652 <ravitejamrt.165@gmail.com>
2023-12-13 13:10:21 +0530

    Home page

--- src/App.js
+++ src/App.js
@@ -3,16 +3,28 @@ import Cart from "./Components/Cart/Cart";
 import NavBar from "./Components/Header/NavBar";
 import ItemsList from "./Components/List/ItemsList";
 import ContextProvider from "./Store/ContextProvider";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import About from "./Components/Header/About";
+import Home from "./Components/Header/Home";
 
 function App() {
-  const [isShown, setIsShown] = useState(false)
+  const [isShown, setIsShown] = useState(false);
+
+  const router = createBrowserRouter([
+    {path: '/about', element: <About />},
+    {path: '/', element: <ItemsList />},
+    {path: '/home', element: <Home />},
+  ]);
+
   const cartEnableHandler = (bool) => {
     setIsShown((prev) => !prev)
-  }
+  };
+
   return (
     <ContextProvider>
+      <RouterProvider router={router} />
       <NavBar cartOnClick={cartEnableHandler} />
-      <ItemsList />
+      
       {isShown && <Cart />}
     </ContextProvider>
   );
---
+++ src/Components/Header/Home.js
@@ -0,0 +1,40 @@
+import React from "react";
+import './Home.css';
+import { Button } from "react-bootstrap";
+
+const listArr = [
+    {   date: 'july 16', 
+        place: 'DETROIT, MI',
+        specPlace: 'DTE ENERGY MUSIC THEATER',
+    },
+    {   date: 'july 19', 
+        place: 'PHEONIX, AZ',
+        specPlace: 'AK CHIN PAVILION',
+    },
+    {   date: 'july 22', 
+        place: 'LAS VEGAS, NV',
+        specPlace: 'T-MOBILE ARENA',
+    },
+]
+
+const Home = () => {
+    const lists = listArr.map(list => (
+        <li className="tour-item">{list.date} {list.place} {list.specPlace}  
+            <Button variant="info">Get Tickets</Button>
+        </li>
+    ))
+    return(
+        <div>
+            <header className="header">
+                <h1 >The Generics</h1>
+                <Button variant="outline-info">Get Our Latest Album</Button>
+            </header>
+            <ul className="tours">
+                    {lists}
+            </ul>
+            
+        </div>
+    )
+};
+
+export default Home;
