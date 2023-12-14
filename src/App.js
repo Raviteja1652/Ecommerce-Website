78e06d18152387e03544587cc04abe5a5f5a1607
Raviteja1652 <ravitejamrt.165@gmail.com>
2023-12-14 12:03:59 +0530

    Contact Us page

--- src/App.js
+++ src/App.js
@@ -7,15 +7,37 @@ import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import About from "./Components/Header/About";
 import Home from "./Components/Header/Home";
 import MoviesApp from "./Components/Movies/MoviesApp";
+import ContactUs from "./Components/Header/ContactUs";
 
 function App() {
   const [isShown, setIsShown] = useState(false);
+  const addDetailHandler = async (detail) => {
+    const res = await fetch('https://react-http-7e214-default-rtdb.firebaseio.com/ContactDetails.json', {
+      method: 'POST',
+      body: JSON.stringify(detail),
+      headers: {
+        'Contact-Type': 'application/json'
+      }
+    });
+    const data = await res.json();
+    const convertedData = [];
+    for (const key in data) {
+      convertedData.push({
+        id: key,
+        name: data[key].name,
+        email: data[key].email,
+        phnNo: data[key].phnNo
+      });
+    }
+    console.log(convertedData)
+  }
 
   const router = createBrowserRouter([
     {path: '/about', element: <About />},
     {path: '/', element: <ItemsList />},
     {path: '/home', element: <Home />},
     {path: '/movies', element: <MoviesApp />},
+    {path: '/contact', element: <ContactUs onAddDetail={addDetailHandler} />},
   ]);
 
   const cartEnableHandler = (bool) => {
---
+++ src/Components/Header/ContactUs.js
@@ -0,0 +1,40 @@
+import React, { useRef } from 'react';
+import './ContactUs.css'
+
+function ContactUs(props) {
+    const nameRef = useRef('');
+    const emailRef = useRef('');
+    const phnNoRef = useRef('');
+
+    const submitHandler = (e) => {
+        e.preventDefault();
+        const detail = {
+            name: nameRef.current.value,
+            email: emailRef.current.value,
+            phnNo: phnNoRef.current.value,
+        };
+        props.onAddDetail(detail);
+    };
+
+  return (
+    <section className='section'>
+        <form onSubmit={submitHandler}>
+            <div className='styleClass'>
+                <label htmlFor='name'>Name: </label>
+                <input type='text' id='name' ref={nameRef} />
+            </div>
+            <div className='styleClass'>
+                <label htmlFor='email'>Email ID: </label>
+                <input type='email' id='email' ref={emailRef} />
+            </div>
+            <div className='styleClass'>
+                <label htmlFor='phnNo'>Phone Number: </label>
+                <input type='number' id='phnNo' ref={phnNoRef} />
+            </div>
+            <button>Submit</button>
+        </form>
+    </section>
+  )
+};
+
+export default ContactUs;
