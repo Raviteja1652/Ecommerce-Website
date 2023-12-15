c41581b1b17d45c65d65b1f99e2fd8a26d7b386e
Raviteja1652 <ravitejamrt.165@gmail.com>
2023-12-15 14:00:20 +0530

    Login Context

--- src/Store/ContextProvider.js
+++ src/Store/ContextProvider.js
@@ -2,16 +2,37 @@ import React, { useState } from "react";
 import cartContext from "./cart-context";
 
 const ContextProvider = props => {
+    const initialToken = localStorage.getItem('token')
+    const [token, setToken] = useState(initialToken)
     const [products, setProducts] = useState([])
+    // const userIsLoggedIn = !!token
+    const [userIsLoggedIn, setUserIsLoggedIn] = useState(false)
 
     const addItemToCartHandler = (item) => {
         setProducts([...products, item])
         // console.log(products)
+    };
+
+    const updateItemsHandler = (index) => {
+        setProducts((prev) => {
+            const updatedItems = [...prev]
+            updatedItems[index].quantity += 1
+            return updatedItems
+        })
+    };
+    const loginHandler = (token) => {
+        localStorage.setItem('token', token)
+        setToken(token)
+        setUserIsLoggedIn(true)
     }
 
     const contextItems = {
         items: products,
-        addToCart: addItemToCartHandler
+        addToCart: addItemToCartHandler,
+        updateItem: updateItemsHandler,
+        token: token,
+        isLoggedIn: userIsLoggedIn,
+        login: loginHandler,
     }
 
     return(
